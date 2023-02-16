eafd3aeb68035d389f706d1449d33cbcf33dd89b
Robel Bekele <robba836@gmail.com>
2023-02-16 13:30:46 +0300

    Task 8 Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unlistd.h>
+#include <unistd.h>
 /**
- * main - A program prints exactly and that piece of art is useful
- * Return: 1 (Correct)
+ *main - A c program prints exactly and that piece of art is useful
+ *Return: 1 (Correct)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n ", stdout);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ", stdout);
+return (1);
 }
