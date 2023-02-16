d22d56cbfa37a9683b25ce4c09e239867183aceb
Robel Bekele <robba836@gmail.com>
2023-02-16 12:15:35 +0300

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A with proper gramer, but the outcome is a piece of art.
+ * Return: 0 (Complete)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
