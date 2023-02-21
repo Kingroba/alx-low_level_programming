40198455a1fa75f9b7b594ab7fcc8abe8d49fada
Robel Bkele <robba836@gmail.com>
2023-02-21 13:34:01 -0800

    Task 8 Rev 1

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	times_table();
+	jack_bauer();
 	return (0);
 }
