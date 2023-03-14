81ac986f6f6c9f604d40ad2963de5b0916793a77
Robel Bkele <robba836@gmail.com>
2023-03-14 14:24:10 -0700

    Rename _putchar to .c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,15 +9,15 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int ci;
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
+	 (s2 == NULL)
 		s2 = "";
 		i = ci = 0;
 	while (s1[i] != '\0')
-		i++
+		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
