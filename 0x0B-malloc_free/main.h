7c17449880d8d33b68aefa22641c86a7ddb98676
Robel Bkele <robba836@gmail.com>
2023-03-16 14:09:57 -0700

    Update main and _putchar

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-	#include <stdlib.h>
-	#include <limits.h>
-
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
@@ -12,4 +8,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+
 #endif
