4948ad68b8e9160980965601c966f8559b1e81e4
Robel Bekele <robba836@gmail.com>
2023-06-16 12:03:14 -0700

    task 4 rev

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * free_dlistint - Frees a dlistint_t list.
+ * free_dlistint - Frees a dlistint_t linked list.
  * @head: Pointer to the head of the list.
  */
 void free_dlistint(dlistint_t *head)
